b4bd3e9b979511085d6a6e4d7586d4cebb54f615
Michael Sweeney <jfmike@gmail.com>
2020-02-27 09:38:42 -0800

    changed json

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,11 @@
 import React from "react"
 import "../styles/App.css"
 
-export default props => <h1>Hello World</h1>
+export default props => {
+  return (
+    <div>
+      <img src="/assets/100_1.jpg" />
+      <h1>Hello World</h1>
+    </div>
+  )
+}
