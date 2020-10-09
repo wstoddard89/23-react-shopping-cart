5ac4c3d8368f088fc40cd6522bcc30b40bb259cc
Will Stoddard <wstoddard89@gmail.com>
2020-10-08 20:30:14 -0700

    work on styling

--- shopping-cart/src/features/products/Products.js
+++ shopping-cart/src/features/products/Products.js
@@ -10,17 +10,36 @@ export function Products() {
     dispatch(getProducts())
   }, [])
 
+  const installments = (a,b) => a / b
+
   return (
     <div className="main-container">
+      <div className="filter-sidebar">
+        {/* {products.map((item) => (
+          <div>
+            {item.availableSizes}
+          </div>
+        ))} */}
+      </div>
     <div className="container">
       <ul className="product-list">
         {products.map((item) => (
+          
           <li
             style={{ backgroundImage: `url(${item.img.normal})` }}
             className="product-list-item"
           >
+            <div className="free-shipping">{item.isFreeShipping}Free Shipping</div>
             <div className="item-title">{item.title}</div>
-            <div className="item-price">{item.price}</div>
+            <div className="item-price">
+            <div><span className="dollar-sign">{item.currencyFormat}</span>{item.price}</div>
+              <div className="installments">
+                <span>Or {item.installments} x</span>
+                <span className="installments-price">
+                  {/* {installments({item.installments}, {item.price})} */}
+                </span>
+              </div>
+            </div>
             <button className="cart-btn">Add to cart</button>
           </li>
         ))}
